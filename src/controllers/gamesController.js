dc688e176822ece399fc39e581524faf48f920a5
joltzz <guijsilva02@gmail.com>
2022-05-27 10:51:41 -0300

    function getGames fixed

---
+++ src/controllers/gamesController.js
@@ -0,0 +1,30 @@
+import db from "../db.js";
+
+export async function getGames(req, res) {
+
+    const {name} = req.query;
+
+    try{
+        if(!name){
+            const {rows: games}=await db.query(`
+               SELECT games.*, categories.name as "categoryName"
+               FROM games
+               JOIN categories ON games."categoryId"=categories.id
+            `);
+            res.send(games);
+        }
+        else{
+            const {rows: games}= await db.query(`
+                SELECT games.*, categories.name as "categoryName"
+                FROM games
+                JOIN categories ON games."categoryId"=categories.id
+                WHERE LOWER(games.name) LIKE LOWER($1)
+            `, [`${name}%`]);
+            res.send(games);
+        }
+    }
+    catch(err){
+        console.log(err);
+        res.send(500);
+    }
+}
